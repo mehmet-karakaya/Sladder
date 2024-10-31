765ec2e3361731813bd8e6aab480f551b074229d
Mehmet Karakaya <m.karakaya.github@gmail.com>
2024-10-31 21:32:43 +0300

    Movement added for player icons (Res Button will be fixed next)

--- sladder.py
+++ sladder.py
@@ -13,6 +13,7 @@ p2Place = 0
 p3Place = 0
 p4Place = 0
 p5Place = 0
+pUptPlace = 0
 p1Name = "p1"
 p2Name = "p2"
 p3Name = "p3"
@@ -27,8 +28,30 @@ currPlayer = playerNames[i]
 inputtxt = ""
 inputFromUser = ""
 frame = ""
+jumpPoint = 0
 upperSpots = [[20, 39], [30, 53], [37, 65], [59, 78], [69, 86], [85, 94]]
 lowerSpots = [[96, 2], [54, 7], [25, 9], [41, 21], [70, 31], [83, 38], [79, 42], [92, 64]]
+# to move +1 to x: +61, to move +1 to y: -51
+certainSpots = [[61, -102], [-122,-153], [61, -153], [61, -102], [-61, -102], [-183, -102], \
+				[-244, 510], [0, 255], [246, 102], [0, 102], [0, 153], [-122, 253], [0, 153], [61, 153]]
+startPoints = [	[35, 735, 50, 750], \
+				[55, 735, 70, 750], \
+				[35, 752, 50, 767], \
+				[55, 752, 70, 767], \
+				[72, 742, 87, 757]]
+# turuncu az sol az yukarı
+#mavi ve yeşil az yukarı
+colors = [	["#9c5708", "#f05133"], \
+			["#9c5708", "#ffd200"], \
+			["#9c5708", "#2d91f0"], \
+			["#9c5708", "#136f13"], \
+			["#9c5708", "#eda727"]]
+redPlayer = ""
+yellowPlayer = ""
+bluePlayer = ""
+greenPlayer = ""
+orangePlayer = ""
+playerCircles = [redPlayer, yellowPlayer, bluePlayer, greenPlayer, orangePlayer]
 
 
 def dice():
@@ -38,62 +61,84 @@ def dice():
 	return diceRolled
 
 def checkUpper(place):
-	global upperSpots
+	global upperSpots, jumpPoint
 	for x in range(len(upperSpots)):
 		if place == upperSpots[x][0]:
+			jumpPoint = x
 			return upperSpots[x][1]
 	return -1
 
 def checkLower(place):
-	global lowerSpots
+	global lowerSpots, upperSpots, jumpPoint
 	for x in range(len(lowerSpots)):
 		if place == lowerSpots[x][0]:
+			jumpPoint = len(upperSpots) + x
 			return lowerSpots[x][1]
 	return -1
 
 def checkSpots(player, place):
-	global p1Place, p2Place, p3Place, p4Place, p5Place, p1Name, p2Name, p3Name, p4Name, p5Name
+	global playerCount, playerNames, p1Name, p2Name, p3Name, p4Name, p5Name, maxPlayer, currPlayer, startPoints, canvasImage, \
+		colors, playerCircles, redPlayer, yellowPlayer, bluePlayer, greenPlayer, orangePlayer, p1Place, p2Place, p3Place, p4Place, p5Place, pUptPlace, certainSpots, jumpPoint
 	x = checkUpper(place)
 	if x < 0:
 		x = checkLower(place)
 	if x > 0:
 		if player == p1Name:
+			canvasImage.move(redPlayer, certainSpots[jumpPoint][0], certainSpots[jumpPoint][1])
 			p1Place = x
 		elif player == p2Name:
+			canvasImage.move(yellowPlayer, certainSpots[jumpPoint][0], certainSpots[jumpPoint][1])
 			p2Place = x
 		elif player == p3Name:
+			canvasImage.move(bluePlayer, certainSpots[jumpPoint][0], certainSpots[jumpPoint][1])
 			p3Place = x
 		elif player == p4Name:
+			canvasImage.move(greenPlayer, certainSpots[jumpPoint][0], certainSpots[jumpPoint][1])
 			p4Place = x
 		elif player == p5Name:
+			canvasImage.move(orangePlayer, certainSpots[jumpPoint][0], certainSpots[jumpPoint][1])
 			p5Place = x
 
 def setPlayers(playerName):
-	global playerCount, playerNames, p1Name, p2Name, p3Name, p4Name, p5Name, maxPlayer, currPlayer
+	global playerCount, playerNames, p1Name, p2Name, p3Name, p4Name, p5Name, maxPlayer, currPlayer, startPoints, \
+		colors, playerCircles, redPlayer, yellowPlayer, bluePlayer, greenPlayer, orangePlayer
 	if playerCount > maxPlayer:
 		playerCount -= 1
 		return
 	if playerCount == 1:
 		p1Name = playerName
 		playerNames[0] = p1Name
+		redPlayer = canvasImage.create_oval(startPoints[0], outline=colors[0][0], fill=colors[0][1], width=2)
+		playerCircles[0] = redPlayer
 	elif playerCount == 2:
 		p2Name = playerName
 		playerNames[1] = p2Name
+		yellowPlayer = canvasImage.create_oval(startPoints[1], outline=colors[1][0], fill=colors[1][1], width=2)
+		playerCircles[1] = yellowPlayer
 	elif playerCount == 3:
 		p3Name = playerName
 		playerNames[2] = p3Name
+		bluePlayer = canvasImage.create_oval(startPoints[2], outline=colors[2][0], fill=colors[2][1], width=2)
+		playerCircles[2] = bluePlayer
 	elif playerCount == 4:
 		p4Name = playerName
 		playerNames[3] = p4Name
+		greenPlayer = canvasImage.create_oval(startPoints[3], outline=colors[3][0], fill=colors[3][1], width=2)
+		playerCircles[3] = greenPlayer
 	elif playerCount == 5:
 		p5Name = playerName
 		playerNames[4] = p5Name
+		orangePlayer = canvasImage.create_oval(startPoints[4], outline=colors[4][0], fill=colors[4][1], width=2)
+		playerCircles[4] = orangePlayer
 	currPlayer = playerNames[0]
 
 def playerTurn(player):
-	global p1Place, p2Place, p3Place, p4Place, p5Place, p1Name, p2Name, p3Name, p4Name, p5Name, rolledDice
+	global p1Place, p2Place, p3Place, p4Place, p5Place, p1Name, p2Name, p3Name, p4Name, p5Name, rolledDice, pUptPlace
 	if player == p1Name:
-		p1Place += dice()
+		moveDist = dice()
+		pUptPlace = p1Place
+		p1Place += moveDist
+		movePlayer(player, moveDist)
 		rolledDiceText = rolledDice.cget("text")
 		rolledDiceText = p1Name + " " + rolledDiceText
 		rolledDice.config(text=rolledDiceText)
@@ -103,7 +148,10 @@ def playerTurn(player):
 			return
 		checkSpots(player, p1Place)
 	elif player == p2Name:
-		p2Place += dice()
+		moveDist = dice()
+		pUptPlace = p2Place
+		p2Place += moveDist
+		movePlayer(player, moveDist)
 		rolledDiceText = rolledDice.cget("text")
 		rolledDiceText = p2Name + " " + rolledDiceText
 		rolledDice.config(text=rolledDiceText)
@@ -113,7 +161,10 @@ def playerTurn(player):
 			return
 		checkSpots(player, p2Place)
 	elif player == p3Name:
-		p3Place += dice()
+		moveDist = dice()
+		pUptPlace = p3Place
+		p3Place += moveDist
+		movePlayer(player, moveDist)
 		rolledDiceText = rolledDice.cget("text")
 		rolledDiceText = p3Name + " " + rolledDiceText
 		rolledDice.config(text=rolledDiceText)
@@ -123,7 +174,10 @@ def playerTurn(player):
 			return
 		checkSpots(player, p3Place)
 	elif player == p4Name:
-		p4Place += dice()
+		moveDist = dice()
+		pUptPlace = p4Place
+		p4Place += moveDist
+		movePlayer(player, moveDist)
 		rolledDiceText = rolledDice.cget("text")
 		rolledDiceText = p4Name + " " + rolledDiceText
 		rolledDice.config(text=rolledDiceText)
@@ -133,7 +187,10 @@ def playerTurn(player):
 			return
 		checkSpots(player, p4Place)
 	elif player == p5Name:
-		p5Place += dice()
+		moveDist = dice()
+		pUptPlace = p5Place
+		p5Place += moveDist
+		movePlayer(player, moveDist)
 		rolledDiceText = rolledDice.cget("text")
 		rolledDiceText = p5Name + " " + rolledDiceText
 		rolledDice.config(text=rolledDiceText)
@@ -152,9 +209,10 @@ def nextPlayerTurn():
 	currPlayer = playerNames[i]
 
 def rollDiceBtnFunc():
-	global currPlayer, p1Place, p2Place, p3Place, p4Place, p5Place, p1Name, p2Name, p3Name, p4Name, p5Name
-	playerTurn(currPlayer)
-	print(p1Name + ": " + str(p1Place) + ", " + p2Name + ": " + str(p2Place) + ", " + p3Name + ": " + str(p3Place) + ", " + p4Name + ": " + str(p4Place) + ", " + p5Name + ": " + str(p5Place))
+	global currPlayer, p1Place, p2Place, p3Place, p4Place, p5Place, p1Name, p2Name, p3Name, p4Name, p5Name, playerCount
+	if playerCount > 0:	
+		playerTurn(currPlayer)
+		print(p1Name + ": " + str(p1Place) + ", " + p2Name + ": " + str(p2Place) + ", " + p3Name + ": " + str(p3Place) + ", " + p4Name + ": " + str(p4Place) + ", " + p5Name + ": " + str(p5Place))
 
 def getInput():
 	global inputtxt, frame, inputFromUser
@@ -165,8 +223,11 @@ def getInput():
 	return
 
 def addPlayerBtnFunc():
-	global playerCount, inputtxt, frame
+	global playerCount, maxPlayer, inputtxt, frame
+	if playerCount >= maxPlayer:
+		return
 	playerCount += 1
+
 	# Take new player's name from user
 	frame = tk.Tk() 
 	frame.title("TextBox Input") 
@@ -188,10 +249,30 @@ def addPlayerBtnFunc():
 	printButton.pack()
 
 def removePlayerBtnFunc():
-	global playerCount, currPlayer, playerNames
-	currPlayer = playerNames[0]
+	global playerCount, currPlayer, playerNames, playerCircles, redPlayer, yellowPlayer, bluePlayer, greenPlayer, orangePlayer, canvasImage
 	if playerCount > 0:
 		playerCount -= 1
+		if playerCount == 0:
+			canvasImage.delete(redPlayer)
+			redPlayer = ""
+			playerNames[0] = redPlayer
+		elif playerCount == 1:
+			canvasImage.delete(yellowPlayer)
+			yellowPlayer = ""
+			playerNames[1] = yellowPlayer
+		elif playerCount == 2:
+			canvasImage.delete(bluePlayer)
+			bluePlayer = ""
+			playerNames[2] = bluePlayer
+		elif playerCount == 3:
+			canvasImage.delete(greenPlayer)
+			greenPlayer = ""
+			playerNames[3] = greenPlayer
+		elif playerCount == 4:
+			canvasImage.delete(orangePlayer)
+			orangePlayer = ""
+			playerNames[4] = orangePlayer
+	resBtnFunc()
 
 def resBtnFunc():
 	global p1Place, p2Place, p3Place, p4Place, p5Place, playerNames, currPlayer
@@ -202,6 +283,155 @@ def resBtnFunc():
 	p5Place = 0
 	currPlayer = playerNames[0]
 	return
+
+
+def movePlayer(player, moveDist):
+	global playerCount, playerNames, p1Name, p2Name, p3Name, p4Name, p5Name, maxPlayer, currPlayer, startPoints, \
+		colors, playerCircles, redPlayer, yellowPlayer, bluePlayer, greenPlayer, orangePlayer, p1Place, p2Place, p3Place, p4Place, p5Place, pUptPlace
+	# X: 61(1 square right), Y: 51 (1 square down)
+	if moveDist <= 0:
+		return
+	if player == p1Name:
+		if (pUptPlace == 10 or pUptPlace == 20 or pUptPlace == 30 or pUptPlace == 40 or pUptPlace == 50 or pUptPlace == 60 or pUptPlace == 70 or pUptPlace == 86 or pUptPlace == 92):
+			moveBy(redPlayer, "-y")
+		elif (0 < pUptPlace < 10 or 20 < pUptPlace < 30 or 40 < pUptPlace < 50 or 60 < pUptPlace < 70 or 80 < pUptPlace < 86 or 92 < pUptPlace < 98):
+			moveBy(redPlayer, "x")
+		elif (10 < pUptPlace < 20 or 30 < pUptPlace < 40 or 50 < pUptPlace < 60 or 70 < pUptPlace < 80 or 86 < pUptPlace < 92):
+			moveBy(redPlayer, "-x")
+		elif (pUptPlace == 80):
+			moveBy(redPlayer, "2x-y")
+		elif (pUptPlace == 98):
+			moveBy(redPlayer, "-2.5x-y")
+		elif (pUptPlace == 99):
+			moveBy(redPlayer, "-y")
+			return
+		pUptPlace += 1
+		movePlayer(player, moveDist-1)
+	elif player == p2Name:
+		if (pUptPlace == 10 or pUptPlace == 20 or pUptPlace == 30 or pUptPlace == 40 or pUptPlace == 50 or pUptPlace == 60 or pUptPlace == 70 or pUptPlace == 86 or pUptPlace == 92):
+			moveBy(yellowPlayer, "-y")
+		elif (0 < pUptPlace < 10 or 20 < pUptPlace < 30 or 40 < pUptPlace < 50 or 60 < pUptPlace < 70 or 80 < pUptPlace < 86 or 92 < pUptPlace < 98):
+			moveBy(yellowPlayer, "x")
+		elif (10 < pUptPlace < 20 or 30 < pUptPlace < 40 or 50 < pUptPlace < 60 or 70 < pUptPlace < 80 or 86 < pUptPlace < 92):
+			moveBy(yellowPlayer, "-x")
+		elif (pUptPlace == 80):
+			moveBy(yellowPlayer, "2x-y")
+		elif (pUptPlace == 98):
+			moveBy(yellowPlayer, "-2.5x-y")
+		elif (pUptPlace == 99):
+			moveBy(yellowPlayer, "-y")
+			return
+		pUptPlace += 1
+		movePlayer(player, moveDist-1)
+	elif player == p3Name:
+		if (pUptPlace == 10 or pUptPlace == 20 or pUptPlace == 30 or pUptPlace == 40 or pUptPlace == 50 or pUptPlace == 60 or pUptPlace == 70 or pUptPlace == 86 or pUptPlace == 92):
+			moveBy(bluePlayer, "-y")
+		elif (0 < pUptPlace < 10 or 20 < pUptPlace < 30 or 40 < pUptPlace < 50 or 60 < pUptPlace < 70 or 80 < pUptPlace < 86 or 92 < pUptPlace < 98):
+			moveBy(bluePlayer, "x")
+		elif (10 < pUptPlace < 20 or 30 < pUptPlace < 40 or 50 < pUptPlace < 60 or 70 < pUptPlace < 80 or 86 < pUptPlace < 92):
+			moveBy(bluePlayer, "-x")
+		elif (pUptPlace == 80):
+			moveBy(bluePlayer, "2x-y")
+		elif (pUptPlace == 98):
+			moveBy(bluePlayer, "-2.5x-y")
+		elif (pUptPlace == 99):
+			moveBy(bluePlayer, "-y")
+			return
+		pUptPlace += 1
+		movePlayer(player, moveDist-1)	
+	elif player == p4Name:
+		if (pUptPlace == 10 or pUptPlace == 20 or pUptPlace == 30 or pUptPlace == 40 or pUptPlace == 50 or pUptPlace == 60 or pUptPlace == 70 or pUptPlace == 86 or pUptPlace == 92):
+			moveBy(greenPlayer, "-y")
+		elif (0 < pUptPlace < 10 or 20 < pUptPlace < 30 or 40 < pUptPlace < 50 or 60 < pUptPlace < 70 or 80 < pUptPlace < 86 or 92 < pUptPlace < 98):
+			moveBy(greenPlayer, "x")
+		elif (10 < pUptPlace < 20 or 30 < pUptPlace < 40 or 50 < pUptPlace < 60 or 70 < pUptPlace < 80 or 86 < pUptPlace < 92):
+			moveBy(greenPlayer, "-x")
+		elif (pUptPlace == 80):
+			moveBy(greenPlayer, "2x-y")
+		elif (pUptPlace == 98):
+			moveBy(greenPlayer, "-2.5x-y")
+		elif (pUptPlace == 99):
+			moveBy(greenPlayer, "-y")
+			return
+		pUptPlace += 1
+		movePlayer(player, moveDist-1)
+	elif player == p5Name:
+		if (pUptPlace == 10 or pUptPlace == 20 or pUptPlace == 30 or pUptPlace == 40 or pUptPlace == 50 or pUptPlace == 60 or pUptPlace == 70 or pUptPlace == 86 or pUptPlace == 92):
+			moveBy(orangePlayer, "-y")
+		elif (0 < pUptPlace < 10 or 20 < pUptPlace < 30 or 40 < pUptPlace < 50 or 60 < pUptPlace < 70 or 80 < pUptPlace < 86 or 92 < pUptPlace < 98):
+			moveBy(orangePlayer, "x")
+		elif (10 < pUptPlace < 20 or 30 < pUptPlace < 40 or 50 < pUptPlace < 60 or 70 < pUptPlace < 80 or 86 < pUptPlace < 92):
+			moveBy(orangePlayer, "-x")
+		elif (pUptPlace == 80):
+			moveBy(orangePlayer, "2x-y")
+		elif (pUptPlace == 98):
+			moveBy(orangePlayer, "-2.5x-y")
+		elif (pUptPlace == 99):
+			moveBy(orangePlayer, "-y")
+			return
+		pUptPlace += 1
+		movePlayer(player, moveDist-1)
+	return
+
+def moveBy(playerCirc, moveRotation):
+	global playerCount, playerNames, p1Name, p2Name, p3Name, p4Name, p5Name, maxPlayer, currPlayer, startPoints, \
+		colors, playerCircles, redPlayer, yellowPlayer, bluePlayer, greenPlayer, orangePlayer, canvasImage, pUptPlace
+	if (playerCirc == redPlayer):
+		if (moveRotation == "-y"):
+			canvasImage.move(redPlayer, 0, -51)
+		elif (moveRotation == "x"):
+			canvasImage.move(redPlayer, 61, 0)
+		elif (moveRotation == "-x"):
+			canvasImage.move(redPlayer, -61, 0)
+		elif (moveRotation == "2x-y"):
+			canvasImage.move(redPlayer, 122, -51)
+		elif (moveRotation == "-2.5x-y"):
+			canvasImage.move(redPlayer, -152, -51)
+	elif (playerCirc == yellowPlayer):
+		if (moveRotation == "-y"):
+			canvasImage.move(yellowPlayer, 0, -51)
+		elif (moveRotation == "x"):
+			canvasImage.move(yellowPlayer, 61, 0)
+		elif (moveRotation == "-x"):
+			canvasImage.move(yellowPlayer, -61, 0)
+		elif (moveRotation == "2x-y"):
+			canvasImage.move(yellowPlayer, 122, -51)
+		elif (moveRotation == "-2.5x-y"):
+			canvasImage.move(yellowPlayer, -152, -51)
+	elif (playerCirc == bluePlayer):
+		if (moveRotation == "-y"):
+			canvasImage.move(bluePlayer, 0, -51)
+		elif (moveRotation == "x"):
+			canvasImage.move(bluePlayer, 61, 0)
+		elif (moveRotation == "-x"):
+			canvasImage.move(bluePlayer, -61, 0)
+		elif (moveRotation == "2x-y"):
+			canvasImage.move(bluePlayer, 122, -51)
+		elif (moveRotation == "-2.5x-y"):
+			canvasImage.move(bluePlayer, -152, -51)
+	elif (playerCirc == greenPlayer):
+		if (moveRotation == "-y"):
+			canvasImage.move(greenPlayer, 0, -51)
+		elif (moveRotation == "x"):
+			canvasImage.move(greenPlayer, 61, 0)
+		elif (moveRotation == "-x"):
+			canvasImage.move(greenPlayer, -61, 0)
+		elif (moveRotation == "2x-y"):
+			canvasImage.move(greenPlayer, 122, -51)
+		elif (moveRotation == "-2.5x-y"):
+			canvasImage.move(greenPlayer, -152, -51)
+	elif (playerCirc == orangePlayer):
+		if (moveRotation == "-y"):
+			canvasImage.move(orangePlayer, 0, -51)
+		elif (moveRotation == "x"):
+			canvasImage.move(orangePlayer, 61, 0)
+		elif (moveRotation == "-x"):
+			canvasImage.move(orangePlayer, -61, 0)
+		elif (moveRotation == "2x-y"):
+			canvasImage.move(orangePlayer, 122, -51)
+		elif (moveRotation == "-2.5x-y"):
+			canvasImage.move(orangePlayer, -152, -51)
+
 
 buttonNames = ["Restart Game", "Add Player", "Remove Player", "Roll Dice"]
 buttonFunctions = [resBtnFunc, addPlayerBtnFunc, removePlayerBtnFunc, rollDiceBtnFunc]
@@ -248,7 +478,7 @@ path = "./img/snake_and_ladder_map.jpg"
 
 img = ImageTk.PhotoImage(Image.open(path).resize((675, 800)))
 
-# Add image to the Canvas Items
+# Add image to the left Canvas
 canvasImage.create_image(0,0, anchor=NW, image=img)
 
 '''
@@ -256,7 +486,7 @@ canvasImage.create_image(0,0, anchor=NW, image=img)
 resBtn = tk.Button(canvasButtons, text="Restart Game", height=2, width=20, anchor=W, command= resBtnFunc)
 canvasButtons_window = canvasButtons.create_window(0, 0, anchor=NW, window=resBtn)
 '''
-
+# Add Buttons and Rolled Dice Label to the Right Canvas
 for i in range(len(buttonNames)):
 	# Create a Button widget for each button
 	button = tk.Button(canvasButtons, text=buttonNames[i], height=2, width=20, anchor=W, command= buttonFunctions[i])
@@ -264,37 +494,6 @@ for i in range(len(buttonNames)):
 # Rolled Dice Label
 rolledDice = tk.Label(canvasButtons, text="Rolled Dice: " + str(diceRolled), height=2, width=20, anchor=W)
 rolledDice.grid(row=len(buttonNames), column=0, padx=10, pady=10)
-
-greenPlayer = canvasImage.create_oval(35, 735, 55, 755, outline="#f11", fill="#1f1", width=1)
-
-"""
-#Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
-img = ImageTk.PhotoImage(Image.open(path).resize((675, 800)))
-
-#The Label widget is a standard Tkinter widget used to display a text or image on the screen.
-board = tk.Label(window, image = img)
-board.grid(row=0, column=0, rowspan=5, padx=0, pady=0)
-
-# Add Restart Button to the right of the screen
-resBtn = tk.Button(window, text="Restart Game", height=1, width=20, command= resBtnFunc)
-resBtn.grid(row=0, column=1, rowspan=1, columnspan=1, padx=0, pady=0)
-
-# Add Player
-addPlayerBtn = tk.Button(window, text="Add Player", height=1, width=20, command= addPlayerBtnFunc)
-addPlayerBtn.grid(row=1, column=1, rowspan=1, columnspan=1, padx=0, pady=0)
-
-# Remove Player
-removePlayerBtn = tk.Button(window, text="Remove Player", height=1, width=20, command= removePlayerBtnFunc)
-removePlayerBtn.grid(row=2, column=1, rowspan=1, columnspan=1, padx=0, pady=0)
-
-# Roll Dice Button
-rollDiceBtn = tk.Button(window, text="Roll Dice", height=1, width=20, command = rollDiceBtnFunc)
-rollDiceBtn.grid(row=3, column=1, rowspan=1, columnspan=1, padx=0, pady=0)
-
-# Rolled Dice
-rolledDice = tk.Label(window, text="Rolled Dice: " + str(diceRolled), height=2, width=20)
-rolledDice.grid(row=4, column=1, rowspan=1, columnspan=1, padx=0, pady=0)
-"""
 
 #Start the GUI
 window.mainloop()
